7f766a5353b3cd2b96edbce3e8c45d8efca740bc
ankit951github <mishrag6393@gmail.com>
2022-09-20 19:34:34 +0530

    correcting code of prime.js

--- prime.js
+++ prime.js
@@ -1,6 +1,16 @@
 function checkPrime(num){
-    let isPrime = false;
-    for(let i=0; i<=num; i++){
-        console.log(num);
+    for(let i=2; i<=num; i++){
+        if(num%i===0){
+            return false;
+        }
     }
+    return true;
 }; 
+
+let val = checkPrime(13);
+if(val){
+    console.log("prime")
+}
+else{
+    console.log("not prime");
+}
